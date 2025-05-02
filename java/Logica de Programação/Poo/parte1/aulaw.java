97cfd0895e1572d762c6ac1ffc94ebe36745e21f
Caio justino <144128768+CaioJustin@users.noreply.github.com>
2025-05-02 19:46:07 -0300

    Add files via upload
    Criando classe,atributo,construtor,getter,setter,metodo, e colocando visibilidade

---
+++ java/Logica de Programação/Poo/parte1/aulaw.java
@@ -0,0 +1,27 @@
+package a;
+
+public class aulaw {
+
+	public static void main(String[]args) {
+		
+	Caneta	C1  =new Caneta("Bic Crystal","Yellow",0.1f,80);
+	Caneta  C2  =new Caneta("Bic Nescal","red",05f,100);
+	
+	
+	//C1.modelo="Bic Crystal";
+	//C1.cor="Azul";
+	//C1.ponta=0;
+	//C1.carga=80;
+	//C1.tampada=true;
+	//C1.destampar();
+	//C1.status();
+	//C1.rabiscar();
+	//C1.pintar();
+	
+	C1.status();
+	C2.status();
+	
+	
+	
+	}
+}
